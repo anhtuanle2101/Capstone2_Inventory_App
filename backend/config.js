f1b55168485f99905ed299c81ecbb9f208706e90
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-11-18 18:37:10 -0500

    add config.js for shared configuration for the application

---
+++ backend/config.js
@@ -0,0 +1,32 @@
+// Shared config for the application; can be required many places
+
+require("dotenv").config();
+require("colors");
+
+const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
+
+const PORT = +process.env.PORT || 3001;
+
+// Use dev database ot production database
+function getDatabaseUri(){
+    return (process.env.NODE_ENV === "test")
+        ? "jobly_test"
+        : process.env.DATABASE_URL || "jobly";
+}
+
+// Speed up bcrypt during tests by reducing the number of hashes
+const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 13;
+
+console.log("Jobly Config".green);
+console.log("SECRET_KEY:".yellow, SECRET_KEY);
+console.log("PORT:".yellow, PORT.toString());
+console.log("BCRYPT_WORK_FACTOR".yellow, BCRYPT_WORK_FACTOR);
+console.log("Database:".yellow, getDatabaseUri());
+console.log("----");
+
+module.exports = {
+    SECRET_KEY,
+    PORT,
+    BCRYPT_WORK_FACTOR,
+    getDatabaseUri
+}
